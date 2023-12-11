e199ea0fa898f9499ef136b59f6a607178fda527
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2023-12-11 09:11:46 +0700

    Rename CheckType to ValidationType

--- src/scripts/validates/form.validate.ts
+++ src/scripts/validates/form.validate.ts
@@ -9,7 +9,7 @@ import {
 } from '@/constant';
 
 // Enum
-import { CheckType } from '../enums/enum.enums';
+import { ValidationType } from '../enums/enum.enums';
 
 class Validate {
   /**
@@ -89,7 +89,7 @@ class Validate {
       if (config[key]) {
         config[key].forEach((validationType: string) => {
           // If there are emptyEmail words, continue to consider the isEmpty condition
-          if (validationType === CheckType.Empty && !this.isEmpty(value)) {
+          if (validationType === ValidationType.Empty && !this.isEmpty(value)) {
             formValidation.isValid = false;
             formValidation.errors[key] = MESSAGES.EMPTY;
 
@@ -99,7 +99,7 @@ class Validate {
           // If there are emptyEmail words, continue to consider the isEmpty condition
           if (
             !formValidation.errors[key] &&
-            validationType === CheckType.EmptyEmail &&
+            validationType === ValidationType.EmptyEmail &&
             !this.isEmpty(value)
           ) {
             formValidation.isValid = false;
@@ -111,7 +111,7 @@ class Validate {
           // If there are emptyPassword words, continue to consider the isEmpty condition
           if (
             !formValidation.errors[key] &&
-            validationType === CheckType.EmptyPassword &&
+            validationType === ValidationType.EmptyPassword &&
             !this.isEmpty(value)
           ) {
             formValidation.isValid = false;
@@ -123,7 +123,7 @@ class Validate {
           // If there is an formatEmail word, continue to consider the isValidEmail condition
           if (
             !formValidation.errors[key] &&
-            validationType === CheckType.FormatEmail &&
+            validationType === ValidationType.FormatEmail &&
             !this.isValidEmail(value)
           ) {
             formValidation.isValid = false;
@@ -135,7 +135,7 @@ class Validate {
           // If there is an passwordRule word, continue to consider the isValidPassword condition
           if (
             !formValidation.errors[key] &&
-            validationType === CheckType.PasswordRule &&
+            validationType === ValidationType.PasswordRule &&
             !this.isValidPassword(value)
           ) {
             formValidation.isValid = false;
@@ -147,7 +147,7 @@ class Validate {
           // If there is a number in the name, an error message will be output
           if (
             !formValidation.errors[key] &&
-            validationType === CheckType.Name &&
+            validationType === ValidationType.Name &&
             !this.isValidName(value)
           ) {
             formValidation.isValid = false;
@@ -159,7 +159,7 @@ class Validate {
           // If the length of the phone number is not equal to 10 or the format is wrong, an error message will be output
           if (
             !formValidation.errors[key] &&
-            validationType === CheckType.Phone &&
+            validationType === ValidationType.Phone &&
             !this.isValidPhoneNumber(value)
           ) {
             formValidation.isValid = false;
